03f5098af7d23a4d90418b9613c7414f1e9c25bd
Nguyễn Đức Trung <nguyenductrung6893@gmail.com>
2021-08-29 16:14:28 +0700

    update file create database

--- EzShop_Data/Entities/CategoryTranslation.cs
+++ EzShop_Data/Entities/CategoryTranslation.cs
@@ -14,5 +14,6 @@ namespace EzShop_Data.Entities
         public string LanguageId { set; get; }
         public string SeoAlias { set; get; }
         public Category Category { get; set; }
+        public Language Language { get; set; }
     }
 }
